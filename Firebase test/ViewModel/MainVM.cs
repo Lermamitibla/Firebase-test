ae860a17b042cc42313463a41d9f7fda64064162
Andrey <artemevandrey1@gmail.com>
2021-03-28 14:27:25 +0300

    неудачная попытка пройти регистрацию почта+пароль :)

--- Firebase test/ViewModel/MainVM.cs
+++ Firebase test/ViewModel/MainVM.cs
@@ -4,30 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Firebase_test.ViewModel;
-using Firebase_test.Model;
-using FirebaseAdmin.Auth;
 using System.Windows.Input;
 using Firebase_test.Commands;
+using Firebase.Auth;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Windows;
 
 namespace Firebase_test.ViewModel
 {
     class MainVM : BaseVM
     {
-        public FirebaseAccount Account { get; set;}
-        public string NewPassword { get; set; }
+        private FirebaseAuthProvider authProvider;
+        private string firebaseToken;
+        private string _password = String.Empty;
+        private string _newPassword = String.Empty;
+        private string _email = String.Empty;
+        private string _accountInfo = String.Empty;
+
+        public string NewPassword
+        {
+            get { return _newPassword; }
+            set { if (!_newPassword.Equals(value)) _newPassword = value; OnPropertyChanged(); }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { if (!_email.Equals(value)) _email = value; OnPropertyChanged(); }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { if (!_password.Equals(value)) _password = value; OnPropertyChanged(); }
+        }
+        public string AccountInfo
+        {
+            get { return _accountInfo; }
+            set { if (!_accountInfo.Equals(value)) _accountInfo = value; OnPropertyChanged(); }
+        }
+
 
         public MainVM()
         {
             CreateCommands();
+            authProvider = PrepareAuthProvider();
         }
 
 
         public ICommand LoginButtonCommand { get; private set; }
         private void OnLoginButtonCommandExecuted(object p)
         {
-           
+          Task<FirebaseAuthLink> login = Login();
+            if (login.IsFaulted) MessageBox.Show(login.Exception.Message);
+            if (login.IsCompleted) 
+            {
+                AccountInfo = login.Result.User.ToString();
+                firebaseToken = login.Result.FirebaseToken;
+            } 
         }
-        private bool CanLoginButtonCommandExecute(object p) => true;
+        private bool CanLoginButtonCommandExecute(object p)
+        {
+            return CheckEmailAndPasswordEntered();
+        }
 
         public ICommand ChangePassButtonCommand { get; private set; }
         private void OnChangePassButtonCommandExecuted(object p)
@@ -50,13 +88,59 @@ namespace Firebase_test.ViewModel
         }
         private bool CanDeleteAccButtonCommandExecute(object p) => true;
 
+        public ICommand CreateAccButtonCommand { get; private set; }
+        private void OnCreateAccButtonCommandExecuted(object p)
+        {
+            var create = CreateAccount().ContinueWith(v => MessageBox.Show(v.Status.ToString(), v.Exception.Message.ToString()));
+           // create.Wait();
+            if (create.IsFaulted) MessageBox.Show(create.Exception.Message);
+            if (create.IsCompleted) MessageBox.Show("Account has been created");
 
+
+            
+        }
+        private bool CanCreateAccButtonCommandExecute(object p)
+        {
+            return CheckEmailAndPasswordEntered();
+        }
+
+        private FirebaseAuthProvider PrepareAuthProvider()
+        {
+            var serviceProvider = new ServiceCollection()
+           .AddHttpClient()
+           .BuildServiceProvider();
+
+            var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+            return new FirebaseAuthProvider("SuperApiKey11", httpClientFactory);
+
+        }
         public void CreateCommands()
         {
             LoginButtonCommand = new LambdaCommand(OnLoginButtonCommandExecuted, CanLoginButtonCommandExecute);
             ChangePassButtonCommand = new LambdaCommand(OnChangePassButtonCommandExecuted, CanChangePassCommandExecute);
             RecoveryPassButtonCommand = new LambdaCommand(OnRecoveryPassButtonCommandExecuted, CanRecoveryPassButtonCommandExecute);
             DeleteAccButtonCommand = new LambdaCommand(OnDeleteAccButtonCommandExecuted, CanDeleteAccButtonCommandExecute);
+            CreateAccButtonCommand = new LambdaCommand(OnCreateAccButtonCommandExecuted, CanCreateAccButtonCommandExecute);
         }
+
+        public bool CheckEmailAndPasswordEntered()
+        {
+            if (_email == string.Empty) return false;
+            if (_password == string.Empty) return false;
+            return true;
+        }
+
+        private async Task<FirebaseAuthLink> Login()
+        {
+            return await authProvider.SignInWithEmailAndPasswordAsync(Email, Password);
+        }
+
+        private async Task<FirebaseAuthLink> CreateAccount()
+        {
+            var create = await authProvider.CreateUserWithEmailAndPasswordAsync(_email, _password, "Test", true);
+            
+            return create;
+        }
+
     }
 }
